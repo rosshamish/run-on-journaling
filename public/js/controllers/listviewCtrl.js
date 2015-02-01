e6a72ab9777bf9c1504f5a83c42ea249e6edc984
Ross Anderson <ross.anderson@ualberta.ca>
2015-02-01 13:57:57 -0500

    final push! demo time!

--- public/js/controllers/listviewCtrl.js
+++ public/js/controllers/listviewCtrl.js
@@ -1,5 +1,5 @@
 app.controller('listviewCtrl', ['$scope', '$window', function($scope, $window) {
 
-	$('body').shards([0,0,0,.5],[255,0,0,.2],[255,255,0,.1],10,2,2,.1,true);
-	$scope.lists = ['dev', 'ideas', 'school', 'thoughts'];
+	$scope.lists = ['hackathon idea', 'yesterday on the bus', 'a smart thing rob winters said'];
+
 }]);
