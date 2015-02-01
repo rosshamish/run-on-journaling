230923f1b55655a6deca1e3a8ae35d89866ed287
Ross Anderson <ross.anderson@ualberta.ca>
2015-02-01 06:12:44 -0500

    rename branchCtrl.js -> editorCtrl.js

--- public/js/application.js
+++ public/js/application.js
@@ -12,7 +12,7 @@ app.config(['$routeProvider', function($routeProvider) {
 	}).
 	when('/editor', {
 		templateUrl: '/html/editor.html',
-		controller: 'branchCtrl'
+		controller: 'editorCtrl'
 	}).
 	otherwise({
 		redirectTo: '/html/landing.html'
