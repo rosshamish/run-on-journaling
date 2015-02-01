e3d77158058983a3de2c12a43da45267717bbec7
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-02-01 08:02:44 -0700

    animation partway done

--- public/js/controllers/branchCtrl.js
+++ public/js/controllers/branchCtrl.js
@@ -1,17 +1,49 @@
-app.controller('branchCtrl', ['$scope', '$window', function($scope, $window) {
+app.controller('branchCtrl', ['$scope', '$window', '$timeout', function($scope, $window, $timeout) {
 
 	$scope.branchIndex = 0;
 	$scope.maxBranchIndex = 0;
 
+	function moveMainLeft(jq) {
+		jq.animate({ "left": "-=55%" }, "slow" );
+	}
+
+	function moveLeftLeft(jq) {
+		jq.animate({"left": "-=15%"}, "slow");
+	}
+
+	function moveRightMain(jq) {
+		jq.animate({"left": "-=55%"});
+	}
+
+	function moveMainRight(jq) {
+		jq.animate({'left':'+=30%'}, "slow" );
+	}
+
+
 	$scope.nextBranch = function() {
-		$scope.branchIndex++;
-		$scope.maxBranchIndex = Math.max($scope.branchIndex, $scope.maxBranchIndex);
+
+		moveMainLeft($('.branch-main'));
+		// moveLeftLeft($('.branch-left'));
+		// moveRightMain($('.branch-right'));
+
+		// $timeout(function() {
+		// 	$scope.branchIndex++;
+		// 	$scope.maxBranchIndex = Math.max($scope.branchIndex, $scope.maxBranchIndex);
+		// }, 1000);
+
 	}
 
 	$scope.prevBranch = function() {
+
+		//moveMainRight($('.branch-main'));
+		//moveMainRight($('.branch-side'));
+
+		//$timeout(500);
 		$scope.branchIndex = $scope.branchIndex - 1;
+
+
 		if ($scope.branchIndex < 0) {
-			$window.location = '/#/listview';
+			//$window.location = '/#/listview';
 		}
 	}
 
